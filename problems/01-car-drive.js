a66105fe8721585e46347b136b570e3941f6fdcb
Daniel Lam <Dann.lam@gmail.com>
2022-05-04 11:31:06 -0700

    A buncha stuff

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,4 +1,23 @@
-// Your code here
+class Car { //Blueprint
+	constructor(speed = 0){ //Factory
+	this.speed = speed
+	}
+
+	drive(newSpeed){
+		// console.log(speed)
+		// console.log(this.speed)
+		return this.speed = newSpeed
+	}
+
+
+}
+
+// let car = new Car(); //We have a 'car' that came out of the factory.
+
+// let test1 = car.drive(10);
+// console.log(test1)
+// let result1 = car.speed;
+// console.log(result1)
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,4 +1,33 @@
-// Your code here
+//Create a calculator class
+class Calculator {
+//Initialize the total property to 0 on newly instantiated instance.
+	constructor(total = 0){
+		this.total = total
+	}
+//Instance methods should return all of the total property.
+	add(num){
+		return this.total += num
+	}
+	subtract(num){
+		return this.total -= num
+	}
+	divide(num){
+		return this.total /= num
+	}
+	multiply(num){
+		return this.total *= num
+	}
+
+}
+
+
+//add(num) adds num to the total
+
+//subtract(num) subtracts num from total
+
+//divide(num) divides the total by the num arg.
+
+//multiply(num) multiply total by the num.
 
 
 /*****************************************************************************/
